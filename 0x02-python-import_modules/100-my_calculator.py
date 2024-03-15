d1285f883fe29d714be52cd4842e88327c9ad699
muchemiwanjiru <you@muchemiwanjiru1.com>
2024-03-15 00:42:03 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,43 @@
 #!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+# calculator by john for john's
+if __name__ == '__main__':
 
-if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
-    import sys
-
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+    if len(sys.argv) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>'.format())
+        exit(1)
+    if sys.argv[2] not in '+-*/':
+        print('Unknown operator. Available operators: +, -, * and /'.format())
+        exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+
+    operator = sys.argv[2]
+    if operator == '+':
+        print('{:d} {} {:d} = {:d}'.format(a, operator, b, add(a, b)))
+    elif operator == '-':
+        print('{:d} {} {:d} = {:d}'.format(a, operator, b, sub(a, b)))
+    elif operator == '*':
+        print('{:d} {} {:d} = {:d}'.format(a, operator, b, mul(a, b)))
+    else:
+        print('{:d} {} {:d} = {:d}'.format(a, operator, b, div(a, b)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
